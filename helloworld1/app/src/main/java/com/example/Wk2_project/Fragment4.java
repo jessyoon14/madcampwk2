efbea05ae62ef1ee54929a0c54fd2086d23e5273
HyunseokLee <hs.lee@kaist.ac.kr>
2019-07-09 03:22:27 +0900

    sddg

--- helloworld1/app/src/main/java/com/example/Wk2_project/Fragment4.java
+++ helloworld1/app/src/main/java/com/example/Wk2_project/Fragment4.java
@@ -1,4 +1,32 @@
 package com.example.Wk2_project;
 
-public class Fragment4 {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.example.Wk2_project.Fg1_Contact.ContactaddActivity;
+
+public class Fragment4 extends Fragment {
+    Button logout;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_fragment4, container, false);
+        logout = view.findViewById(R.id.bt_logout);
+        logout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent i = new Intent(view.getContext(),PreActivity.class);
+                startActivity(i);
+            }
+        });
+
+        return view;
+    }
+
 }
